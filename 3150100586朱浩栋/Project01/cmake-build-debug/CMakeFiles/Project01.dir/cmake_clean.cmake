b0229c1ae143d57dada61f2cf947ee71bd089f23
MovingZ <57850861+MovingZ@users.noreply.github.com>
2019-12-09 21:55:05 +0800

    Homework01

---
+++ 3150100586朱浩栋/Project01/cmake-build-debug/CMakeFiles/Project01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project01.dir/main.cpp.obj"
+  "CMakeFiles/Project01.dir/src/glad.c.obj"
+  "Project01.exe"
+  "Project01.exe.manifest"
+  "Project01.pdb"
+  "libProject01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Project01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
